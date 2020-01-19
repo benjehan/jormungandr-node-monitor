508e053f3a38993f08d99a7da5fea89db13315d4
Dogpatch Media <jon@dogpatchmedia.com>
2020-01-19 10:09:17 -0800

    new stuff added
    now tells you the difference between the time it took for you to get the block and shelley to get the block. added a better possible fork detection (well the same really, just a better way of reporting it). fixed some cosmetic issues.

--- monitor.sh
+++ monitor.sh
@@ -16,9 +16,16 @@
 #
 
 ### CONFIGURATION
+# set tab spacing
+tabs 5
+
+# number of blocks behind consesnsus tip before restarting
+GET_BEHIND=100
 
 # block counter set to 0 at startup
+# dont change this or else the world will end
 COUNTER=0
+
 
 # path to jcli
 JCLI="jcli"
@@ -27,34 +34,34 @@ JCLI_PORT=3100
 # path to log file
 LOG_FILE="/home/coconut/logs/stuck.log"
 
-# network stuff (dont touch)
+# network stuff
+# if you change this, oh man, its gonna be bad
 
 LAST_BLOCK=""
+
+# time stuff
 START_TIME=$SECONDS
 
 # how many seconds should we wait if no blocks show up
-RESTART_GT=1360
+# i like about 6 minutes
+RESTART_GT=3600
 
 # display output headers
 echo "/////////////////////////////////////////////////////////////////////////////////////"
 echo "///////////////////////// JORMUNGANDR NODE MONITOR //////////////////////////////////"
 echo "/////////////////////////////////////////////////////////////////////////////////////"
 echo ""
-echo "TODAY DATE | EP | SLOT# | T: EXPLR | T: LOCAL | H: SHL | H: LOC | H: CON | LAST HASH | COUNTER"
+printf "DATE \t EP \t SLOT \t EXP. TIME \t LOCAL TIME \t DIFFS \t SHLLY \t LOCAL \t POOLTL \t HASH \t FORK \t COUNT \n"
+printf "DATE \t EP \t SLOT \t EXP. TIME \t LOCAL TIME \t DIFFS \t SHLLY \t LOCAL \t POOLTL \t HASH \t FORK \t COUNT \n" >> ${LOG_FILE}
 echo ""
-
-# write headers to log file
-echo "/////////////////////////////////////////////////////////////////////////////////////" >> ${LOG_FILE}
-echo "///////////////////////// JORMUNGANDR NODE MONITOR //////////////////////////////////" >> ${LOG_FILE}
-echo "/////////////////////////////////////////////////////////////////////////////////////" >> ${LOG_FILE}
-echo ""  >> ${LOG_FILE}
-echo "TODAY DATE | EP | SLOT# | T: EXPLR | T: LOCAL | H: SHL | H: LOC | H: CON | LAST HASH | COUNTER" >> ${LOG_FILE}
-echo ""  >> ${LOG_FILE}
 
 # start the monitoring
 while true
 do  
     
+    # multiple blocks starting state
+    MULTIBLOCK="NO "
+
     #todays date
     DATE=$(date '+%Y-%m-%d')
 
@@ -62,10 +69,9 @@ do
     MAJOR_TIP=$(curl -s https://pooltool.s3-us-west-2.amazonaws.com/stats/stats.json | jq -r .majoritymax)
 
     # the time your node got the block
-    TIME=$($JCLI rest v0 node stats get --host "http://127.0.0.1:${JCLI_PORT}/api" | grep lastReceivedBlockTime | awk '{print $2}' | cut -c 13- | rev | cut -c 8- | rev)
-    
+    TIME=$($JCLI rest v0 node stats get --host "http://127.0.0.1:${JCLI_PORT}/api" | grep lastReceivedBlockTime | awk '{print $2}' | cut -c 13- | rev | cut -c 8- | rev)    
     # last block hash 9 chars
-    LAST_HASH=$($JCLI rest v0 node stats get --host "http://127.0.0.1:${JCLI_PORT}/api" | grep lastBlockHash | awk '{print $2}' | cut -c 1-9)
+    LAST_HASH=$($JCLI rest v0 node stats get --host "http://127.0.0.1:${JCLI_PORT}/api" | grep lastBlockHash | awk '{print $2}' | cut -c 1-5)
     
     # last block number
     LATEST_BLOCK=$($JCLI rest v0 node stats get --host "http://127.0.0.1:${JCLI_PORT}/api" | grep lastBlockHeight | awk '{print $2}' | rev | cut -c 2- | rev | cut -c 2-)
@@ -90,22 +96,28 @@ do
     
          # if a block isnt shown its probably a double block
         if [ "$LATEST_BLOCK" != $(($LAST_BLOCK+1)) ]; then
-               echo "POSSIBLE MULTIPLE BLOCKS CREATED AT BLOCK:  $(($LAST_BLOCK+1))"
-               echo "POSSIBLE MULTIPLE BLOCKS CREATED AT BLOCK:  $(($LAST_BLOCK+1))" >> ${LOG_FILE}
-            fi
+               
+               MULTIBLOCK="YES"
+        fi
     
          # restart is the node gets too far behind the major_tip 
-         if [ "$LATEST_BLOCK" -lt $(($MAJOR_TIP-10)) ]; then
+         if [ "$LATEST_BLOCK" -lt $(($MAJOR_TIP-$GET_BEHIND)) ]; then
             echo "TOO FAR BEHIND MAJOR TIP. RESTARTING NODE."
             echo "TOO FAR BEHIND MAJOR TIP. RESTARTING NODE." >> ${LOG_FILE}
             sudo service jorg restart
          fi
+        # get last block count from shelley explorer
         shelleyExplorerJson=`curl -X POST -H "Content-Type: application/json" --data '{"query": " query {   allBlocks (last: 3) {    pageInfo { hasNextPage hasPreviousPage startCursor endCursor  }  totalCount  edges {    node {     id  date { slot epoch {  id  firstBlock { id  }  lastBlock { id  }  totalBlocks }  }  transactions { totalCount edges {   node {    id  block { id date {   slot   epoch {    id  firstBlock { id  }  lastBlock { id  }  totalBlocks   } } leader {   __typename   ... on Pool {    id  blocks { totalCount  }  registration { startValidity managementThreshold owners operators rewards {   fixed   ratio {  numerator  denominator   }   maxLimit } rewardAccount {   id }  }   } }  }  inputs { amount address {   id }  }  outputs { amount address {   id }  }   }   cursor }  }  previousBlock { id  }  chainLength  leader { __typename ... on Pool {  id  blocks { totalCount  }  registration { startValidity managementThreshold owners operators rewards {   fixed   ratio {  numerator  denominator   }   maxLimit } rewardAccount {   id }  } }  }    }    cursor  }   } }  "}' https://explorer.incentivized-testnet.iohkdev.io/explorer/graphql 2> /dev/null`
         shelleyLastBlockCount=`echo $shelleyExplorerJson | grep -m 1 -o '"chainLength":"[^"]*' | cut -d'"' -f4 | awk '{print $NF}'`
         shelleyLastBlockCount=`echo $shelleyLastBlockCount|cut -d ' ' -f3`
 
-            echo "${DATE} | ${EPOCH} | ${LATEST_SLOT} | ${LAST_BLOCK_TIME} | ${TIME} | ${shelleyLastBlockCount} | ${LATEST_BLOCK} | ${MAJOR_TIP} | ${LAST_HASH} | ${COUNTER}"
-            echo "${DATE} | ${EPOCH} | ${LATEST_SLOT} | ${LAST_BLOCK_TIME} | ${TIME} | ${shelleyLastBlockCount} | ${LATEST_BLOCK} | ${MAJOR_TIP} | ${LAST_HASH} | ${COUNTER}" >> ${LOG_FILE}
+        # calculate time difference between shelley and local
+        SBT=$(date -d "${LAST_BLOCK_TIME} ${DATE}" +%s)
+        LBT=$(date -d "${TIME} ${DATE}" +%s)
+        TDIFF=$(($SBT-$LBT))
+
+            printf "${DATE} \t ${EPOCH} \t ${LATEST_SLOT} \t ${LAST_BLOCK_TIME} \t ${TIME} \t ${TDIFF}s \t ${shelleyLastBlockCount} \t ${LATEST_BLOCK} \t ${MAJOR_TIP} \t ${LAST_HASH} \t ${MULTIBLOCK} \t ${COUNTER} \n"
+            printf "${DATE} \t ${EPOCH} \t ${LATEST_SLOT} \t ${LAST_BLOCK_TIME} \t ${TIME} \t ${TDIFF}s \t ${shelleyLastBlockCount} \t ${LATEST_BLOCK} \t ${MAJOR_TIP} \t ${LAST_HASH} \t ${MULTIBLOCK} \t ${COUNTER} \n" >> ${LOG_FILE}
             LAST_BLOCK="$LATEST_BLOCK"
  else
             ELAPSED_TIME=$(($SECONDS - $START_TIME))
